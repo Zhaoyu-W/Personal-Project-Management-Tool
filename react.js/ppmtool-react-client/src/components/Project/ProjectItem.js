568ea7e36a7e63bdc16e73ad91a32ee5cfd037d4
WalkerWangUMN <wzy0794@gmail.com>
2020-04-26 23:48:06 -0500

    create&update project

--- react.js/ppmtool-react-client/src/components/Project/ProjectItem.js
+++ react.js/ppmtool-react-client/src/components/Project/ProjectItem.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+
 
 class ProjectItem extends Component {
     render() {
+        const {project} = this.props;
         return (
             <div class="container">
                 <div class="card card-body bg-light mb-3">
                     <div class="row">
                         <div class="col-2">
-                            <span class="mx-auto">REACT</span>
+                            <span class="mx-auto">{project.projectIdentifier}</span>
                         </div>
                         <div class="col-lg-6 col-md-4 col-8">
-                            <h3>Spring / React Project</h3>
-                            <p>Project to create a Kanban Board with Spring Boot and React</p>
+                            <h3>{project.projectName}</h3>
+                            <p>{project.description}</p>
                         </div>
                         <div class="col-md-4 d-none d-lg-block">
                             <ul class="list-group">
@@ -20,11 +23,11 @@ class ProjectItem extends Component {
                                         <i class="fa fa-flag-checkered pr-1"> Project Board </i>
                                     </li>
                                 </a>
-                                <a href="#">
+                                <Link to={`/updateProject/${project.projectIdentifier}`}>
                                     <li class="list-group-item update">
                                         <i class="fa fa-edit pr-1"> Update Project Info</i>
                                     </li>
-                                </a>
+                                </Link>
                                 <a href="">
                                     <li class="list-group-item delete">
                                         <i class="fa fa-minus-circle pr-1"> Delete Project</i>
